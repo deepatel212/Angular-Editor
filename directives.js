7fe105ce63644dc2fcfc5b977c91f7e79082550f
Deep Patel <pateldeep.cse@gmail.com>
2016-04-06 11:15:07 +0530

    x

---
+++ directives.js
@@ -0,0 +1,39 @@
+﻿myApp.directive('jumbotron', function ($compile) {
+    return {
+        restrict: 'E',
+//        scope: { text: '@' },
+        templateUrl: "directives/jumbotron.html",
+        controller: function ($scope, $element) {
+            $scope.add = function () {
+                var el = $compile("<jumbotron></jumbotron>")($scope);
+                $element.parent().append(el);
+            };
+        }
+    };
+});
+myApp.directive('divs', function ($compile) {
+    return {
+        restrict: 'E',
+        //        scope: { text: '@' },
+        templateUrl: "directives/divs.html",
+        controller: function ($scope, $element) {
+            $scope.add = function () {
+                var el = $compile("<divs></divs>")($scope);
+                $element.parent().append(el);
+            };
+        }
+    };
+});
+myApp.directive('htmlcontent', function () {
+    var directive = {};
+
+    directive.restrict = 'E';
+
+    directive.templateUrl = "directives/htmlcontent.html";
+
+    directive.scope = {
+    //        user: "=user"
+}
+
+return directive;
+})
---
+++ index.js
@@ -0,0 +1,25 @@
+﻿var myApp = angular.module('portfolioApp', ['ui.router']);
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/state1");
+
+    $stateProvider
+    .state('state1', {
+        url: "/state1",
+        templateUrl: "views/state1.html",
+        controller: "state1Controller"
+    })
+    .state('state2', {
+        url: "/state2",
+        templateUrl: "views/state2.html",
+        controller: "state2Controller"
+    })
+});
+myApp.controller("mainController", function ($scope,$compile) {
+
+    $scope.insertDiv = function (value) {
+        var compiledeHTML = $compile("<" + value + "></" + value + ">")($scope);
+        $("#mainContent:last-child").append(compiledeHTML); 
+    }
+});
